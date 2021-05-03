8e2616b865ff2ffb443c381022bfd0e001cacb54
insookim0702 <insookim0702@gmail.com>
2021-05-03 21:20:24 +0900

    - 더하기 기능 추가 - 삭제 기능 추가 - 공백 입력시 예외 처리

--- index.js
+++ index.js
@@ -18,37 +18,43 @@
     }
 
     function writeList(todo, id) {
-        TODO_UL.remove;
-        const li = document.createElement('li');
-        const todoValue = document.createElement('span');
-        const btnDelete = document.createElement('span');
-        const btnUpdate = document.createElement('span');
-        li.className = 'todo_li';
-        todoValue.className = 'todo_value';
-        todoValue.textContent = todo;
-        todoValue.id = id;
-        btnDelete.addEventListener('click', deleteTodo);
-        btnDelete.id = id;
-        btnDelete.textContent = 'delete';
-        btnDelete.className = 'btn_delete';
-        // btnUpdate.addEventListener('click', updateTodo);
-        // btnUpdate.className = 'btn_update';
-        li.appendChild(todoValue);
-        li.appendChild(btnDelete);
-        // li.appendChild(btnUpdate);
-        TODO_UL.appendChild(li);
+        if (todo) {
+            const li = document.createElement('li');
+            const todoValue = document.createElement('span');
+            const btnDelete = document.createElement('span');
+            const btnUpdate = document.createElement('span');
+            li.className = 'todo_li';
+            li.id = id;
+            todoValue.className = 'todo_value';
+            todoValue.textContent = todo;
+            todoValue.id = id;
+            btnDelete.addEventListener('click', deleteTodo);
+            btnDelete.id = id;
+            btnDelete.textContent = 'delete';
+            btnDelete.className = 'btn_delete';
+            // btnUpdate.addEventListener('click', updateTodo);
+            // btnUpdate.className = 'btn_update';
+            li.appendChild(todoValue);
+            li.appendChild(btnDelete);
+            // li.appendChild(btnUpdate);
+            TODO_UL.appendChild(li);
+        }
     }
 
     BTN_TODO_SAVE.addEventListener('click', () => {
+        if (TODO.value === '') {
+            alert('ㅗ나 먹어');
+            return;
+        }
         setTodoList(TODO.value);
+        const todoList = JSON.parse(localStorage.getItem('todoList'));
+        writeList(TODO.value, todoList.length);
         TODO.value = '';
-        init();
     });
 
     function setTodoList(todo) {
         let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
         todoList.push(todo);
-        console.log(todoList);
         localStorage.setItem('todoList', JSON.stringify(todoList));
     }
 
@@ -57,6 +63,7 @@
         let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
         todoList.splice(id, 1);
         localStorage.setItem('todoList', JSON.stringify(todoList));
-        init();
+        const li = document.getElementById(deleteId);
+        li.remove();
     }
 })();
